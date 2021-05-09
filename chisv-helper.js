15c12cb36564afb6c88270321f1f4a314421e149
Sören Schröder <code@razupaltuff.eu>
2021-05-09 15:38:55 +0200

    #7 Add delay of 100ms before start calculation
    To prevent that local times are calculated on old data a delay was added before calculation starts.
    Furthermore the delay is reset if another change which is originated in one of the default table columns (i.e. not the columns added by the extension).
    This leads to about 1/4 of the number of executions of the recalculation, which also should improve performance

--- chisv-helper.js
+++ chisv-helper.js
@@ -6,6 +6,7 @@ const ENDS_COLUMN_HEADER = "Ends"
 const ENDS_LOCAL_COLUMN_HEADER = "Ends\xa0local"
 
 let currentlySettingLocalTimes = false
+let calculateLocalTimesTimer
 
 let dateFormat
 let timeFormat
@@ -16,10 +17,8 @@ moment.locale(locale)
 const discoverDateFormat = function () {
     if (!$('#testDate').length) {
         let testDate = moment('1999-12-31 13:00')
-        console.log(testDate.format(''))
         $('.table-wrapper').before(`<span>Format used by chisv-helper extension for parsing: <span id="testDate">${testDate.local().format('l LT')}</span><br/><small>If this format does not match the format in the table please change either the locale in your profile or in the brows. The settings in the profile change how the date and time is displayed in the table. The settings of the browser change which format is used for parsing.</small></span>`)
         let checkDate = $('#testDate').text()
-        $('#testDate').remove()
         let leadingMonth = checkDate.startsWith('12')
         let leadingYear = checkDate.startsWith('1999')
         let isTwelveHourClock = checkDate.includes('PM')
@@ -41,61 +40,59 @@ const discoverDateFormat = function () {
 }
 
 const insertLocalTimes = function () {
-    let $tasksTab = $(`nav.tabs>ul>li:contains(${TASK_TAB_TITLE})`)
-    let tasksTabIndex = $tasksTab.index()
-    let taskTabContent = $(`section.tab-content > div:nth-child(${tasksTabIndex + 1})`)
-    let tableHead = taskTabContent.find('thead')
+    calculateLocalTimesTimer = window.setTimeout(function () {
+        let $tasksTab = $(`nav.tabs > ul > li:contains(${TASK_TAB_TITLE})`)
+        let tasksTabIndex = $tasksTab.index()
+        let taskTabContent = $(`section.tab-content > div:nth-child(${tasksTabIndex + 1})`)
+        let tableHead = taskTabContent.find('thead')
 
-    let startsHead = tableHead.find(`div:contains(${START_COLUMN_HEADER})`).closest('th')
-    if (!tableHead.find(`div:contains('${STARTS_LOCAL_COLUMN_HEADER}')`).length) {
-        let startsLocalHead = startsHead.clone()
-        startsLocalHead.find(`div:contains(${START_COLUMN_HEADER})`).text(STARTS_LOCAL_COLUMN_HEADER)
-        startsLocalHead.append(`<small style="font-size: .8rem; color:gray">${moment.tz.guess()}</small>`)
-        startsLocalHead.removeClass('is-sortable')
-        startsHead.after(startsLocalHead)
-    }
-
-    let endsHead = tableHead.find(`div:contains(${ENDS_COLUMN_HEADER})`).closest('th')
-    if (!tableHead.find(`div:contains('${ENDS_LOCAL_COLUMN_HEADER}')`).length) {
-        let endsLocalHead = endsHead.clone()
-        endsLocalHead.find(`div:contains(${ENDS_COLUMN_HEADER})`).text(ENDS_LOCAL_COLUMN_HEADER)
-        endsLocalHead.append(`<small style="font-size: .8rem; color:gray">${moment.tz.guess()}</small>`)
-        endsLocalHead.removeClass('is-sortable')
-        endsHead.after(endsLocalHead)
-    }
-
-    let tableBody = taskTabContent.find('tbody')
-    tableBody.children('tr').each(function (index, tr) {
-        let dateText = $(tr).find("[data-label='Date']").text()
-        let startCell = $(tr).find("[data-label='Starts']")
-        let startText = startCell.text()
-        let endCell = $(tr).find("[data-label='Ends']")
-        let endText = endCell.text()
-
-
-        let start = moment.tz(dateText + " " + startText, `${dateFormat} ${timeFormat}`, "Asia/Tokyo")
-        let end = moment.tz(dateText + " " + endText, `${dateFormat} ${timeFormat}`, "Asia/Tokyo")
-
-        let localStartsCell = $(tr).find("[data-label='Starts local']")
-        if (!localStartsCell.length) {
-            localStartsCell = startCell.clone()
-            localStartsCell.attr("data-label", "Starts local")
-            startCell.after(localStartsCell)
+        let startsHead = tableHead.find(`div:contains(${START_COLUMN_HEADER})`).closest('th')
+        if (!tableHead.find(`div:contains('${STARTS_LOCAL_COLUMN_HEADER}')`).length) {
+            let startsLocalHead = startsHead.clone()
+            startsLocalHead.find(`div:contains(${START_COLUMN_HEADER})`).text(STARTS_LOCAL_COLUMN_HEADER)
+            startsLocalHead.append(`<small style="font-size: .8rem; color:gray">${moment.tz.guess()}</small>`)
+            startsLocalHead.removeClass('is-sortable')
+            startsHead.after(startsLocalHead)
         }
-        localStartsCell.text(start.local().format('LT'))
-        localStartsCell.append(`<br/><small style="font-size: .8rem; color:gray">${start.local().format('dd,\xa0l')}</small>`)
 
-        let localEndsCell = $(tr).find("[data-label='Ends local']")
-        if (!localEndsCell.length) {
-            localEndsCell = endCell.clone()
-            localEndsCell.attr("data-label", "Ends local")
-            endCell.after(localEndsCell)
+        let endsHead = tableHead.find(`div:contains(${ENDS_COLUMN_HEADER})`).closest('th')
+        if (!tableHead.find(`div:contains('${ENDS_LOCAL_COLUMN_HEADER}')`).length) {
+            let endsLocalHead = endsHead.clone()
+            endsLocalHead.find(`div:contains(${ENDS_COLUMN_HEADER})`).text(ENDS_LOCAL_COLUMN_HEADER)
+            endsLocalHead.append(`<small style="font-size: .8rem; color:gray">${moment.tz.guess()}</small>`)
+            endsLocalHead.removeClass('is-sortable')
+            endsHead.after(endsLocalHead)
         }
-        localEndsCell.text(end.local().format('LT'))
-        localEndsCell.append(`<br/><small style="font-size: .8rem; color:gray">${end.local().format('dd,\xa0l')}</small>`)
 
-    })
-    window.setTimeout(function () {
+        let tableBody = taskTabContent.find('tbody')
+        tableBody.children('tr').each(function (index, tr) {
+            let dateText = $(tr).find("[data-label='Date']").text().trim()
+            let startCell = $(tr).find("[data-label='Starts']")
+            let startText = startCell.text().trim()
+            let endCell = $(tr).find("[data-label='Ends']")
+            let endText = endCell.text().trim()
+
+            let start = moment.tz(dateText + " " + startText, `${dateFormat} ${timeFormat}`, "Asia/Tokyo")
+            let end = moment.tz(dateText + " " + endText, `${dateFormat} ${timeFormat}`, "Asia/Tokyo")
+
+            let localStartsCell = $(tr).find("[data-label='Starts local']")
+            if (!localStartsCell.length) {
+                localStartsCell = startCell.clone()
+                localStartsCell.attr("data-label", "Starts local")
+                startCell.after(localStartsCell)
+            }
+            localStartsCell.text(start.local().format('LT'))
+            localStartsCell.append(`<br/><small style="font-size: .8rem; color:gray">${start.local().format('dd,\xa0l')}</small>`)
+
+            let localEndsCell = $(tr).find("[data-label='Ends local']")
+            if (!localEndsCell.length) {
+                localEndsCell = endCell.clone()
+                localEndsCell.attr("data-label", "Ends local")
+                endCell.after(localEndsCell)
+            }
+            localEndsCell.text(end.local().format('LT'))
+            localEndsCell.append(`<br/><small style="font-size: .8rem; color:gray">${end.local().format('dd,\xa0l')}</small>`)
+        })
         currentlySettingLocalTimes = false
     }, 100)
 }
@@ -108,14 +105,20 @@ const config = {attributes: true, attributeFilter: ['class'], childList: true, s
 
 // Callback function to execute when mutations are observed
 const callback = function (mutationsList, observer) {
+    let nodesChangedByApp = mutationsList.filter(m => !["Starts local", "Ends local"].includes(m.target?.attributes["data-label"]?.nodeValue))
     let $conferenceTabNavigation = $('nav.tabs')
-    if (!currentlySettingLocalTimes && $conferenceTabNavigation.length) {
+    if (!currentlySettingLocalTimes && $conferenceTabNavigation.length && nodesChangedByApp.length) {
         currentlySettingLocalTimes = true
         discoverDateFormat()
         insertLocalTimes();
-        $(".is-sortable").on('click', function(event){
+        $(".is-sortable").on('click', function (event) {
+            window.clearTimeout(calculateLocalTimesTimer)
             insertLocalTimes()
         });
+    }
+    if (currentlySettingLocalTimes && $conferenceTabNavigation.length && nodesChangedByApp.length) {
+        window.clearTimeout(calculateLocalTimesTimer)
+        insertLocalTimes()
     }
 };
 
